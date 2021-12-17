1e909b9e2a55d6055e7ce085788dfd02b07e1016
gogowonji <65698313+gogowonji@users.noreply.github.com>
2021-12-17 17:24:24 +0900

    server, client 파일 수정/ 메인 액티비티 수정

--- app/src/main/java/com/example/chatprogram/Client.java
+++ app/src/main/java/com/example/chatprogram/Client.java
@@ -1,8 +1,13 @@
 package com.example.chatprogram;
 
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,38 +16,43 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] arg)
+
+    public void client()
     {
+
         Socket socket = null;            //Server와 통신하기 위한 Socket
         BufferedReader in = null;        //Server로부터 데이터를 읽어들이기 위한 입력스트림
         BufferedReader in2 = null;        //키보드로부터 읽어들이기 위한 입력스트림
         PrintWriter out = null;            //서버로 내보내기 위한 출력 스트림
         InetAddress ia = null;              //주소
+        String msg;
+        MainActivity main = new MainActivity();
+
         try {
             ia = InetAddress.getByName("192.168.219.100");    //서버로 접속
             socket = new Socket(ia,9000);
-            // TODO: Thread
+            //Thread
             Thread thread1 = new SenderThread(socket);
             Thread thread2 = new ReceiverThread(socket);
             thread1.start();
             thread2.start();
+
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            in2 = new BufferedReader(new InputStreamReader(System.in));
+            //in2 = new BufferedReader(new InputStreamReader(System.in));
             out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
 
-            System.out.println(socket.toString());
+            //System.out.println(socket.toString());
         }catch(IOException e) {
-            System.out.println(e.getMessage());
-
+           Log.i("client","error");
         }
 
         try {
-            System.out.print("클라이언트 : ");
-            String data = in2.readLine();          //키보드로부터 입력
-            out.println(data);                        //서버로 데이터 전송
+
+            String data = main.msg;//데이터 가져오기
+            out.println(data);//서버로 데이터 전송
             out.flush();
-            //String str2 = in.readLine();            //서버로부터 되돌아오는 데이터 읽어들임
-            //System.out.println("서버로부터 되돌아온 메세지 : " + str2);
+            String str2 = in.readLine();//서버로부터 되돌아오는 데이터 읽어들임
+            main.clientTv.setText(str2);
             socket.close();
 
         }catch(IOException e) {}
--- app/src/main/java/com/example/chatprogram/MainActivity.java
+++ app/src/main/java/com/example/chatprogram/MainActivity.java
@@ -1,25 +1,21 @@
 package com.example.chatprogram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
-    TextView msg;
+    EditText chat;
+    public String msg;
+    TextView clientTv;
+    TextView serverTv;
+
 
 
     @Override
@@ -28,12 +24,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void SubmitClick(View v){
-        msg = (TextView) findViewById(R.id.client);
-        Intent intent =
-                new Intent(MainActivity.this, Client.class);
-        //startActivity(intent);
-        startActivityForResult(intent, 1);
 
+
+    public void SubmitClick(View v){
+        chat = (EditText) findViewById(R.id.chat);
+        msg = chat.getText().toString();
+        chat.setText("");
+        if(msg != null){
+            Server s = new Server();
+            Client c = new Client();
+            s.server();
+            c.client();
+        }
     }
 }
